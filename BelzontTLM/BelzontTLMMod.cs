478861be0482614b9a89f08b6fb04c20c0b93204
Leandro Klyte <klyte45@hotmail.com>
2023-06-16 09:56:00 -0300

    Initial version

---
+++ BelzontTLM/BelzontTLMMod.cs
@@ -0,0 +1,23 @@
+﻿using Game;
+using Game.Modding;
+using System.Reflection;
+
+[assembly: AssemblyVersion("0.0.0.*")]
+namespace BelzontTLM
+{
+    public class BelzontTLMMod : IMod
+    {
+        public void OnCreateWorld(UpdateSystem updateSystem)
+        {
+
+        }
+
+        public void OnDispose()
+        {
+        }
+
+        public void OnLoad()
+        {
+        }
+    }
+}
---
+++ BelzontTLM_EUIS/BTLM_EUIS.cs
@@ -0,0 +1,25 @@
+﻿using K45EUIS_Ext;
+using System;
+using System.Collections.Generic;
+
+namespace BelzontTLM_EUIS
+{
+    public class BTLM_EUIS : IEUISAppRegister
+    {
+        public string AppName => "@k45/btlm";
+
+        public string DisplayName => "Transport Lines Manager Belzont Edition";
+
+        public string UrlJs => "coui://k45.euis/UI/k45-euis-root.js";
+
+        public string UrlIcon => "coui://gameui/Media/Game/Icons/TramLine.svg";
+
+        public string ModderIdentifier => "k45";
+
+        public string ModAcronym => "tlmb";
+
+        public Dictionary<string, Delegate> EventsToBind => new Dictionary<string, Delegate>();
+
+        public Dictionary<string, Delegate> CallsToBind => new Dictionary<string, Delegate>();
+    }
+}
