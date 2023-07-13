612ad636ac7fdfa900b9879fcf70031c92b22d3f
Leandro Klyte <klyte45@hotmail.com>
2023-07-13 18:12:06 -0300

    Normalizing preset palettes declaration format

--- BelzontTLM/Palette/XTMPresetPalettes.cs
+++ BelzontTLM/Palette/XTMPresetPalettes.cs
@@ -7,175 +7,20 @@ namespace BelzontTLM.Palettes
 {
     internal static class XTMPresetPalettes
     {
-        public static readonly List<Color32> SaoPaulo2035 = new List<Color32>(new Color32[]{
-            new Color32 (0, 13, 160, 255),
-            new Color32 (0, 128, 27, 255),
-            new Color32 (250, 0, 0, 255),
-            new Color32 (255, 213, 3, 255),
-            new Color32 (165, 67, 153, 255),
-            new Color32 (244, 115, 33, 255),
-            new Color32 (159, 24, 102, 255),
-            new Color32 (158, 158, 148, 255),
-            new Color32 (0, 168, 142, 255),
-            new Color32 (4, 124, 140, 255),
-            new Color32 (240, 78, 35, 255),
-            new Color32 (4, 43, 106, 255),
-            new Color32 (0, 172, 92, 255),
-            new Color32 (30, 30, 30, 255),
-            new Color32 (180, 178, 177, 255),
-            new Color32 (255, 255, 255, 255),
-            new Color32 (245, 158, 55, 255),
-            new Color32 (167, 139, 107, 255),
-            new Color32 (0, 149, 218, 255),
-            new Color32 (252, 124, 161, 255),
-            new Color32 (95, 44, 143, 255),
-            new Color32 (92, 58, 14, 255),
-            new Color32 (0, 0, 0, 255),
-            new Color32 (100, 100, 100, 255),
-            new Color32 (202, 187, 168, 255),
-            new Color32 (0, 0, 255, 255),
-            new Color32 (208, 45, 255, 255),
-            new Color32 (0, 255, 0, 255),
-            new Color32 (255, 252, 186, 255),
-            new Color32 (117, 0, 0, 255),
-        });
-        public static readonly List<Color32> London2016 = new List<Color32>(new Color32[]{
-            new Color32 (220,36,31,255),
-            new Color32 (225,206,0,255),
-            new Color32 (0,114,41,255),
-            new Color32 (215,153,175,255),
-            new Color32 (134,143,152,255),
-            new Color32 (117,16,86,255),
-            new Color32 (0,0,0,255),
-            new Color32 (0,25,168,255),
-            new Color32 (0,160,226,255),
-            new Color32 (118,208,189,255),
-            new Color32 (102,204,0,255),
-            new Color32 (232,106,16,255),
-            new Color32 (137,78,36,255),
-        });
-        public static readonly List<Color32> Rainbow = new List<Color32>(new Color32[]{
-            new Color32 (   36,12 ,243 ,255),
-            new Color32 (   56,73 ,245 ,255),
-            new Color32 (   85,156,246 ,255),
-            new Color32 (  111,233,179 ,255),
-            new Color32 (   93,201, 97 ,255),
-            new Color32 (   80,170, 40 ,255),
-            new Color32 (   81,164, 25 ,255),
-            new Color32 (  115,195, 29 ,255),
-            new Color32 (  152,220, 31 ,255),
-            new Color32 (  249,254, 41 ,255),
-            new Color32 (  233,222, 36 ,255),
-            new Color32 (  227,194, 33 ,255),
-            new Color32 (  219,161, 32 ,255),
-            new Color32 (  202, 96, 26 ,255),
-            new Color32 (  192, 49, 24 ,255),
-            new Color32 (  189,  3, 23 ,255),
-            new Color32 (  133,  0, 28 ,255),
-            new Color32 (   73,  1, 63 ,255),
-            new Color32 (   44,  4, 94 ,255),
-            new Color32 (   25,12 ,243 ,255),
-        });
-
-        public static readonly List<Color32> RainbowShort = new List<Color32>(new Color32[]{
-            new Color32 ( 130,  0,220  ,255),
-            new Color32 (  30, 60,255  ,255),
-            new Color32 (   0,160,255  ,255),
-            new Color32 (   0,200,200  ,255),
-            new Color32 (   0,210,140  ,255),
-            new Color32 (   0,220,  0  ,255),
-            new Color32 ( 160,230, 50  ,255),
-            new Color32 ( 230,220, 50  ,255),
-            new Color32 ( 230,175, 45  ,255),
-            new Color32 ( 240,130, 40  ,255),
-            new Color32 ( 250, 60, 60  ,255),
-            new Color32 ( 240,  0,130  ,255),
-            new Color32 ( 160,  0,200  ,255),
-        });
-
-
-        public static readonly List<Color32> WorldMix = new List<Color32>(new Color32[]{
-            new Color32 (230, 25, 75     ,255),
-            new Color32 (60, 180, 75     ,255),
-            new Color32 (255, 225, 25    ,255),
-            new Color32 (0, 130, 200     ,255),
-            new Color32 (245, 130, 48    ,255),
-            new Color32 (145, 30, 180    ,255),
-            new Color32 (70, 240, 240    ,255),
-            new Color32 (240, 50, 230    ,255),
-            new Color32 (210, 245, 60    ,255),
-            new Color32 (250, 190, 190 ,255),
-            new Color32 (0, 128, 128     ,255),
-            new Color32 (230, 190, 255 ,255),
-            new Color32 (170, 110, 40    ,255),
-            new Color32 (255, 250, 200 ,255),
-            new Color32 (128, 0, 0 ,255),
-            new Color32 (170, 255, 195 ,255),
-            new Color32 (128, 128, 0     ,255),
-            new Color32 (255, 215, 180 ,255),
-            new Color32 (0, 0, 128      ,255),
-            new Color32 (128, 128, 128 ,255),
-            new Color32 (255, 255, 255 ,255),
-            new Color32 (0, 0, 0        ,255),
-        });
-
-        public static readonly List<Color32> MSMetroUI = new List<Color32>(new Color32[]{
-            new Color32  (162, 0, 255, 255),
-            new Color32  (27, 161, 226, 255),
-            new Color32  (140, 191, 38, 255),
-            new Color32  (229, 20, 0, 255),
-            new Color32  (255, 0, 151, 255),
-            new Color32  (230, 113, 184, 255),
-            new Color32  (160, 80, 0, 255),
-            new Color32  (0, 171, 169, 255),
-            new Color32  (240, 150, 9, 255),
-            new Color32   (51,153,51,255),
-        });
-
-        public static readonly List<Color32> MatColor100 = new List<Color32>(new Color32[]{
-           new Color32(0xff,0xcd,0xd2,255),    new Color32(0xf8,0xbb,0xd0,255),    new Color32(0xe1,0xbe,0xe7,255),    new Color32(0xd1,0xc4,0xe9,255),    new Color32(0xc5,0xca,0xe9,255),    new Color32(0xbb,0xde,0xfb,255),    new Color32(0xb3,0xe5,0xfc,255),    new Color32(0xb2,0xeb,0xf2,255),    new Color32(0xb2,0xdf,0xdb,255),    new Color32(0xc8,0xe6,0xc9,255),    new Color32(0xdc,0xed,0xc8,255),    new Color32(0xf0,0xf4,0xc3,255),    new Color32(0xff,0xf9,0xc4,255),    new Color32(0xff,0xec,0xb3,255),    new Color32(0xff,0xe0,0xb2,255),    new Color32(0xff,0xcc,0xbc,255),    new Color32(0xd7,0xcc,0xc8,255),    new Color32(0xf5,0xf5,0xf5,255),    new Color32(0xcf,0xd8,0xdc,255),new Color32(0xcf,0xd8,0xdc,255),
-      });
-        public static readonly List<Color32> MatColor500 = new List<Color32>(new Color32[]{
-    new Color32(0xf4,0x43,0x36,255),   new Color32(0xe9,0x1e,0x63,255),    new Color32(0x9c,0x27,0xb0,255),    new Color32(0x67,0x3a,0xb7,255),    new Color32(0x3f,0x51,0xb5,255),    new Color32(0x21,0x96,0xf3,255),    new Color32(0x03,0xa9,0xf4,255),    new Color32(0x00,0xbc,0xd4,255),    new Color32(0x00,0x96,0x88,255),    new Color32(0x4c,0xaf,0x50,255),    new Color32(0x8b,0xc3,0x4a,255),    new Color32(0xcd,0xdc,0x39,255),    new Color32(0xff,0xeb,0x3b,255),    new Color32(0xff,0xc1,0x07,255),    new Color32(0xff,0x98,0x00,255),    new Color32(0xff,0x57,0x22,255),    new Color32(0x79,0x55,0x48,255),    new Color32(0x9e,0x9e,0x9e,255),    new Color32(0x60,0x7d,0x8b,255),new Color32(0x60,0x7d,0x8b,255),
-      });
-        public static readonly List<Color32> MatColor900 = new List<Color32>(new Color32[]{
-new Color32(0xb7,0x1c,0x1c,255),   new Color32(0x88,0x0e,0x4f,255),    new Color32(0x4a,0x14,0x8c,255),    new Color32(0x31,0x1b,0x92,255),    new Color32(0x1a,0x23,0x7e,255),    new Color32(0x0d,0x47,0xa1,255),    new Color32(0x01,0x57,0x9b,255),    new Color32(0x00,0x60,0x64,255),    new Color32(0x00,0x4d,0x40,255),    new Color32(0x1b,0x5e,0x20,255),    new Color32(0x33,0x69,0x1e,255),    new Color32(0x82,0x77,0x17,255),    new Color32(0xf5,0x7f,0x17,255),    new Color32(0xff,0x6f,0x00,255),    new Color32(0xe6,0x51,0x00,255),    new Color32(0xbf,0x36,0x0c,255),    new Color32(0x3e,0x27,0x23,255),    new Color32(0x21,0x21,0x21,255),    new Color32(0x26,0x32,0x38,255),    new Color32(0x26,0x32,0x38,255),
-        });
-        public static readonly List<Color32> MatColorA200 = new List<Color32>(new Color32[]{
-   new Color32(0xff,0x52,0x52,255),  new Color32(0xff,0x40,0x81,255),    new Color32(0xe0,0x40,0xfb,255),    new Color32(0x7c,0x4d,0xff,255),    new Color32(0x53,0x6d,0xfe,255),    new Color32(0x44,0x8a,0xff,255),    new Color32(0x40,0xc4,0xff,255),    new Color32(0x18,0xff,0xff,255),    new Color32(0x64,0xff,0xda,255),    new Color32(0x69,0xf0,0xae,255),    new Color32(0xb2,0xff,0x59,255),    new Color32(0xee,0xff,0x41,255),    new Color32(0xff,0xff,0x00,255),    new Color32(0xff,0xd7,0x40,255),    new Color32(0xff,0xab,0x40,255),new Color32(0xff,0x6e,0x40,255),
-       });
-        public static readonly List<Color32> MatColorA400 = new List<Color32>(new Color32[]{
-   new Color32(0xff,0x17,0x44,255),   new Color32(0xf5,0x00,0x57,255),    new Color32(0xd5,0x00,0xf9,255),    new Color32(0x65,0x1f,0xff,255),    new Color32(0x3d,0x5a,0xfe,255),    new Color32(0x29,0x79,0xff,255),    new Color32(0x00,0xb0,0xff,255),    new Color32(0x00,0xe5,0xff,255),    new Color32(0x1d,0xe9,0xb6,255),    new Color32(0x00,0xe6,0x76,255),    new Color32(0x76,0xff,0x03,255),    new Color32(0xc6,0xff,0x00,255),    new Color32(0xff,0xea,0x00,255),    new Color32(0xff,0xc4,0x00,255),    new Color32(0xff,0x91,0x00,255),new Color32(0xff,0x3d,0x00,255),
-       });
-        public static readonly List<Color32> MatColorA700 = new List<Color32>(new Color32[]{
-   new Color32(0xd5,0x00,0x00,255),  new Color32(0xc5,0x11,0x62,255),    new Color32(0xaa,0x00,0xff,255),    new Color32(0x62,0x00,0xea,255),    new Color32(0x30,0x4f,0xfe,255),    new Color32(0x29,0x62,0xff,255),    new Color32(0x00,0x91,0xea,255),    new Color32(0x00,0xb8,0xd4,255),    new Color32(0x00,0xbf,0xa5,255),    new Color32(0x00,0xc8,0x53,255),    new Color32(0x64,0xdd,0x17,255),    new Color32(0xae,0xea,0x00,255),    new Color32(0xff,0xd6,0x00,255),    new Color32(0xff,0xab,0x00,255),    new Color32(0xff,0x6d,0x00,255),new Color32(0xdd,0x2c,0x00,255),
-        });
-
-        public static readonly List<Color32> CPTM_SP_2000 = new List<Color32>(new Color32[]{
-            new Color32   (124,97,78,255),
-            new Color32  (150, 154, 153, 255),
-            new Color32  (77, 140, 211, 255),
-            new Color32  (212, 184, 136, 255),
-            new Color32  (222, 142, 5, 255),
-            new Color32  (67, 39, 123, 255),
-            new Color32  (154, 54, 124, 255),
-            new Color32  (3, 170, 87, 255),
-            new Color32  (14, 14, 14, 255),
-            new Color32  (93, 47, 145, 255),
-        });
-
-        public static readonly List<Color32> SP_BUS_2000 = new List<Color32>(new Color32[]{
-            new Color32  (5,225,31,255),
-            new Color32  (0, 77, 133, 255),
-            new Color32  (255, 245, 0, 255),
-            new Color32  (218, 37, 28, 255),
-            new Color32  (0, 115, 100, 255),
-            new Color32  (0, 114, 184, 255),
-            new Color32  (159, 44, 41, 255),
-            new Color32  (229, 119, 24, 255),
-            new Color32  (200, 200, 200, 255),
-        });
-
+        public static readonly List<Color32> SaoPaulo2035 = "000DA0#00801B#FA0000#FFD503#A54399#F47321#9F1866#9E9E94#00A88E#047C8C#F04E23#042B6A#00AC5C#1E1E1E#B4B2B1#FFFFFF#F59E37#A78B6B#0095DA#FC7CA1#5F2C8F#5C3A0E#000000#646464#CABBA8#0000FF#D02DFF#00FF00#FFFCBA#750000".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> London2016 = "DC241F#E1CE00#007229#D799AF#868F98#751056#000000#0019A8#00A0E2#76D0BD#66CC00#E86A10#894E24".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> Rainbow = "240CF3#3849F5#559CF6#6FE9B3#5DC961#50AA28#51A419#73C31D#98DC1F#F9FE29#E9DE24#E3C221#DBA120#CA601A#C03118#BD0317#85001C#49013F#2C045E#190CF3".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> RainbowShort = "8200DC#1E3CFF#00A0FF#00C8C8#00D28C#00DC00#A0E632#E6DC32#E6AF2D#F08228#FA3C3C#F00082#A000C8".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> WorldMix = "E6194B#3CB44B#FFE119#0082C8#F58230#911EB4#46F0F0#F032E6#D2F53C#FABEBE#008080#E6BEFF#AA6E28#FFFAC8#800000#AAFFC3#808000#FFD7B4#000080#808080#FFFFFF#000000".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MSMetroUI = "A200FF#1BA1E2#8CBF26#E51400#FF0097#E671B8#A05000#00ABA9#F09609#339933".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColor100 = "ffcdd2#f8bbd0#e1bee7#d1c4e9#c5cae9#bbdefb#b3e5fc#b2ebf2#b2dfdb#c8e6c9#dcedc8#f0f4c3#fff9c4#ffecb3#ffe0b2#ffccbc#d7ccc8#f5f5f5#cfd8dc#cfd8dc".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColor500 = "f44336#e91e63#9c27b0#673ab7#3f51b5#2196f3#03a9f4#00bcd4#009688#4caf50#8bc34a#cddc39#ffeb3b#ffc107#ff9800#ff5722#795548#9e9e9e#607d8b#607d8b".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColor900 = "b71c1c#880e4f#4a148c#311b92#1a237e#0d47a1#01579b#006064#004d40#1b5e20#33691e#827717#f57f17#ff6f00#e65100#bf360c#3e2723#212121#263238#263238".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColorA200 = "ff5252#ff4081#e040fb#7c4dff#536dfe#448aff#40c4ff#18ffff#64ffda#69f0ae#b2ff59#eeff41#ffff00#ffd740#ffab40#ff6e40".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColorA400 = "ff1744#f50057#d500f9#651fff#3d5afe#2979ff#00b0ff#00e5ff#1de9b6#00e676#76ff03#c6ff00#ffea00#ffc400#ff9100#ff3d00".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> MatColorA700 = "d50000#c51162#aa00ff#6200ea#304ffe#2962ff#0091ea#00b8d4#00bfa5#00c853#64dd17#aeea00#ffd600#ffab00#ff6d00#dd2c00".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> CPTM_SP_2000 = "7C614E#969A99#4D8CD3#D4B888#DE8E05#43277B#9A367C#03AA57#0E0E0E#5D2F91".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
+        public static readonly List<Color32> SP_BUS_2000 = "05E11F#004D85#FFF500#DA251C#007364#0072B8#9F2C29#E57718#C8C8C8".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
         public static readonly List<Color32> NYC_SUBWAY_1972 = "F26422#E71D2F#00ADC6#D63493#3D332B#E9AF21#2172B9#1BB35F".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
         public static readonly List<Color32> NYC_SUBWAY_1979 = "E71D2F#1BB35F#AC459A#2172B9#F26422#E9AF21#7EBB42#985B25#919693#3D332B#00ADC6".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
         public static readonly List<Color32> NYC_SUBWAY_MODERN_OFFICIAL = "EE352E#00933C#B933AD#0039A6#FF6319#FCCC0A#6CBE45#996633#A7A9AC#808183".Split("#").Select(x => ColorExtensions.FromRGB(x)).ToList();
