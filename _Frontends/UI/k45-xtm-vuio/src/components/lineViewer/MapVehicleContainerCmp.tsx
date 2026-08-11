a8775960ac3f5817d41e9aa62c53e1ab6af12ecb
Leandro Klyte <klyte45@kwyt.com.br>
2026-08-10 21:32:14 -0300

    Enhance vehicle representation with crowdness indicators and visual styles

--- _Frontends/UI/k45-xtm-vuio/src/components/lineViewer/MapVehicleContainerCmp.tsx
+++ _Frontends/UI/k45-xtm-vuio/src/components/lineViewer/MapVehicleContainerCmp.tsx
@@ -1,5 +1,6 @@
 import { Unit } from "#enum/Unit";
 import { VehicleData } from "#service/LineManagementService";
+import { getCrowdnessBorderStyle, getCrowdnessRatio } from "#utility/lineViewerUtils";
 import { nameToString, toVanillaEntity } from "@klyte45/vuio-commons";
 import { useValue } from "cs2/api";
 import { camera, selectedInfo } from "cs2/bindings";
@@ -14,18 +15,58 @@ type Props = {
 
 export function MapVehicleContainerCmp({ vehicle, isFaded }: Props) {
     const selectedEntity = useValue(selectedInfo.selectedEntity$);
+    const ratio = getCrowdnessRatio(vehicle.cargo, vehicle.capacity);
+    const border = getCrowdnessBorderStyle(ratio);
+    const width = border.borderWidthRem + "rem";
+    const color = border.borderColor;
+
+    const fillStyle = {
+        "--vehicleColor": "gray",
+        "--vehicleFill": border.fillPercent + "%",
+        borderTopWidth: width,
+        borderRightWidth: width,
+        borderBottomWidth: width,
+        borderLeftWidth: width,
+        borderTopColor: "black",
+        borderRightColor: "black",
+        borderBottomColor: "black",
+        borderLeftColor: "black",
+        borderTopStyle: "solid",
+        borderRightStyle: "solid",
+        borderBottomStyle: "solid",
+        borderLeftStyle: "solid"
+    } as CSSProperties;
+
+    const ringStyle = {
+        borderTopWidth: width,
+        borderRightWidth: width,
+        borderBottomWidth: width,
+        borderLeftWidth: width,
+        borderTopColor: color,
+        borderRightColor: color,
+        borderBottomColor: color,
+        borderLeftColor: color,
+        borderTopStyle: "solid",
+        borderRightStyle: "solid",
+        borderBottomStyle: "solid",
+        borderLeftStyle: "solid"
+    } as CSSProperties;
+
     return <Tooltip tooltip={`${vehicle.cargo} / ${vehicle.capacity} (${LocalizedNumber.renderString(useLocalization(), {
-        value: vehicle.cargo / vehicle.capacity * 100,
+        value: ratio * 100,
         unit: Unit.PercentageSingleFraction
     })})`}>
-        <div className={["vehicleContainer", [vehicle.entity].some(x => x.Index == selectedEntity.index) ? "xtm-selected" : ""].join(" ")} style={{ top: (vehicle.normalizedPosition * 100) + "%", "--vehicleColor": "gray", "--vehicleFill": (vehicle.cargo / vehicle.capacity * 100) + "%", opacity: isFaded ? 0.5 : 1 } as CSSProperties}
+        <div className={["vehicleContainer", [vehicle.entity].some(x => x.Index == selectedEntity.index) ? "xtm-selected" : ""].join(" ")} style={{ top: (vehicle.normalizedPosition * 100) + "%", "--vehicleColor": "gray", opacity: isFaded ? 0.5 : 1, zIndex: Math.floor(vehicle.normalizedPosition * 100) + 2000 } as CSSProperties}
             onClick={() => {
                 selectedInfo.selectEntity(toVanillaEntity(vehicle.entity))
                 camera.focusEntity(toVanillaEntity(vehicle.entity))
             }}>
             <div className="vehicleNeedle"><div className="painting" /></div>
-            <div className="vehicle" style={{ zIndex: Math.floor(vehicle.normalizedPosition * 100) + 2000 } as CSSProperties}>
-                {nameToString(vehicle.name) + " " + vehicle.entity.Index}
+            <div className="vehicleShell">
+                <div className="vehicle" style={fillStyle}>
+                    {nameToString(vehicle.name) + " " + vehicle.entity.Index}
+                </div>
+                <div className={["vehicleRing", border.pulse && "crowdnessPulse"].join(" ")} style={ringStyle} />
             </div>
         </div>
     </Tooltip >;
